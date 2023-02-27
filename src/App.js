c6b3d23c8b29d954b06da21bdd9fb7c0e04b3554
fromHeisei <masamasa20011018@gmail.com>
2023-02-27 20:13:21 +0900

    new commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { useEffect, useState } from "react";
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 import { BulletListContainer } from "./BulletListContainer";
+import { AddForm } from "./AddForm";
 import { bulletForm } from "./bulletForm";
 
 function App() {
   return (
     <div class="App">
       <header class="App-header">掲示板</header>
+      <BrowserRouter>
+        <Link to="/bulletForm">新規スレッド作成</Link>
+        <Switch>
+          <Route path="/bulletForm">
+            <bulletForm />
+          </Route>
+        </Switch>
+      </BrowserRouter>
       <div class="bullet">
         <h1>新着スレッド</h1>
-        <bulletForm />
+        <AddForm />
         <BulletListContainer />
       </div>
     </div>
