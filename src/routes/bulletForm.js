6f942f6a950b612c60249beebc6548dde7e53f48
fromHeisei <masamasa20011018@gmail.com>
2023-03-01 19:11:52 +0900

    thread create

--- src/routes/bulletForm.js
+++ src/routes/bulletForm.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export function BulletForm() {
+  const [formtext, setFormtext] = useState("");
+
+  const formTextPost = () => {
+    axios.post(
+      "https://2y6i6tqn41.execute-api.ap-northeast-1.amazonaws.com/threads",
+      {
+        title: formtext,
+      }
+    );
+  };
   return (
-    <div>
-      <h1>Hello!</h1>
+    <div class="Form">
+      <input
+        value={formtext}
+        onChange={(event) => setFormtext(event.target.value)}
+      />
+
+      <button onClick={formTextPost}>スレッドを追加</button>
     </div>
   );
 }
