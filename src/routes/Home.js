6f510a6c4b4e2819bcdaf65302f2e3d56c7b88e1
fromHeisei <masamasa20011018@gmail.com>
2023-03-04 21:41:46 +0900

    complete

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -7,7 +7,7 @@ export function Home() {
     <div class="App">
       <header class="App-header">
         <div>掲示板</div>
-        <Link to={"/bulletform/"}>新規スレッド</Link>
+        <Link to={"/bulletform/"}>新規スレッド作成</Link>
       </header>
       <div class="bullet">
         <h1>新着スレッド</h1>
--- src/routes/ThreadPost.js
+++ src/routes/ThreadPost.js
@@ -1,30 +1,30 @@
 import "../App.css";
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useLocation } from "react-router-dom";
 
 export function ThreadPost() {
   const { state } = useLocation();
   const [threadList, setThreadList] = useState([]);
   const [postThread, setPostThread] = useState("");
+  const inputRef = useRef();
 
-  useEffect(() => {
+  const apiFetch = () => {
     fetch(
       `https://2y6i6tqn41.execute-api.ap-northeast-1.amazonaws.com/threads/${state}/posts?offset=0`
     )
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
-        setThreadList(data.posts);
+        if (data.posts != null) {
+          setThreadList(data.posts);
+        }
       });
-  }, [state]);
-  const threadItems = threadList.map((thread) => (
-    <div>
-      <h2 key={thread.id} value={thread.post}>
-        {thread.post}
-      </h2>
-    </div>
-  ));
+  };
+
+  useEffect(() => {
+    apiFetch();
+  }, []);
 
   const clickButton = () => {
     axios.post(
@@ -33,18 +33,29 @@ export function ThreadPost() {
         post: postThread,
       }
     );
-    window.location.reload();
-    alert("投稿しました。");
+    apiFetch();
   };
   return (
     <div>
       <header class="thread-header">投稿一覧</header>
-      <input
-        value={postThread}
-        onChange={(event) => setPostThread(event.target.value)}
-      />
-      <button onClick={clickButton}>投稿する</button>
-      <div class="threadItems">{threadItems}</div>
+      <div class="threadposts">
+        <input
+          ref={inputRef}
+          value={postThread}
+          onChange={(event) => setPostThread(event.target.value)}
+          placeholder="投稿内容"
+        />
+        <button onClick={clickButton}>投稿する</button>
+        <div class="threadItems">
+          {threadList.map((thread) => (
+            <div>
+              <h2 key={thread.id} value={thread.post}>
+                {thread.post}
+              </h2>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
